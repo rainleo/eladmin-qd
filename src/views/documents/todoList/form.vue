7e28f69c5d830d2a2a5a36a69d7e99b16ea2a9d8
sayeed <smileok2006@126.com>
2019-11-15 17:02:19 +0800

    待办

--- src/views/documents/todoList/form.vue
+++ src/views/documents/todoList/form.vue
@@ -1,21 +1,19 @@
 <template>
   <el-dialog :append-to-body="true" :visible.sync="dialog" :title="isAdd ? '新增' : '编辑'" width="500px">
     <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-      <el-form-item label="摘要" >
-        <el-input v-model="form.todoAbstract" style="width: 370px;"/>
+      <el-form-item label="摘要" prop="todoAbstract"><el-input v-model.trim="form.todoAbstract" style="width: 370px;" /></el-form-item>
+      <el-form-item label="预期完成时间" prop="expectedCompletionTime"><el-input v-model="form.expectedCompletionTime" style="width: 370px;" /></el-form-item>
+      <el-form-item label="抄送人" prop="copyPerson" >
+        <el-select v-model="copyPersonId" style="width: 370px;" placeholder="请选择抄送人" filterable @focus="getUsers">
+          <el-option v-for="(item, index) in copyPerson" :key="item.username + index" :label="item.username" :value="item.id" />
+        </el-select>
       </el-form-item>
-      <el-form-item label="预期完成时间" >
-        <el-input v-model="form.expectedCompletionTime" style="width: 370px;"/>
+      <el-form-item label="协助人员" prop="assistantPerson">
+        <el-select v-model="assistantPersonId" style="width: 370px;" placeholder="请选择协助人员" filterable @focus="getUsers">
+          <el-option v-for="(item, index) in assistantPerson" :key="item.username + index" :label="item.username" :value="item.id" />
+        </el-select>
       </el-form-item>
-      <el-form-item label="抄送人" >
-        <el-input v-model="form.copyPersonName" style="width: 370px;"/>
-      </el-form-item>
-      <el-form-item label="协助人员" >
-        <el-input v-model="form.assistantName" style="width: 370px;"/>
-      </el-form-item>
-      <el-form-item label="待办内容" >
-        <el-input v-model="form.content" style="width: 370px;"/>
-      </el-form-item>
+      <el-form-item label="待办内容" prop="content"><el-input v-model.trim="form.content" style="width: 370px;" /></el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button type="text" @click="cancel">取消</el-button>
@@ -26,6 +24,7 @@
 
 <script>
 import { add, edit } from '@/api/todoList'
+import { getUsers } from '@/api/user'
 export default {
   props: {
     isAdd: {
@@ -35,19 +34,21 @@ export default {
   },
   data() {
     return {
-      loading: false, dialog: false,
+      loading: false,
+      dialog: false,
       form: {
-        id: '',
         todoAbstract: '',
         expectedCompletionTime: '',
-        copyPersonName: '',
-        assistantName: '',
-        content: '',
-        createTime: '',
-        updateTime: '',
-        deleted: ''
+        content: ''
       },
+      copyPersonId: null,
+      assistantPersonId: null,
+      copyPerson: [],
+      assistantPerson: [],
       rules: {
+        todoAbstract: [{ required: true, message: '摘要不能为空', trigger: 'blur' }],
+        // expectedCompletionTime: [{ required: true, message: '预期完成时间不能为空', trigger: 'blur' }],
+        content: [{ required: true, message: '待办内容不能为空', trigger: 'blur' }]
       }
     }
   },
@@ -56,60 +57,90 @@ export default {
       this.resetForm()
     },
     doSubmit() {
-      this.loading = true
-      if (this.isAdd) {
-        this.doAdd()
-      } else this.doEdit()
+      this.form.copyPersonId = this.copyPersonId
+      // this.form.assistantPersonId = this.assistantPersonId
+      // 置空，防止关联的user对象不为空，关联修改数据库报错
+      this.form.copyPerson = null
+      this.form.assistantPerson = null
+      this.$refs['form'].validate(valid => {
+        if (valid) {
+          if (this.copyPersonId === null || this.copyPersonId === undefined) {
+            this.$message({
+              message: '抄送人不能为空',
+              type: 'warning'
+            })
+          } else {
+            this.loading = true
+            if (this.isAdd) {
+              this.doAdd()
+            } else this.doEdit()
+          }
+        } else {
+          return false
+        }
+      })
     },
     doAdd() {
-      add(this.form).then(res => {
-        this.resetForm()
-        this.$notify({
-          title: '添加成功',
-          type: 'success',
-          duration: 2500
+      add(this.form)
+        .then(res => {
+          this.resetForm()
+          this.$notify({
+            title: '添加成功',
+            type: 'success',
+            duration: 2500
+          })
+          this.loading = false
+          this.$parent.init()
         })
-        this.loading = false
-        this.$parent.init()
-      }).catch(err => {
-        this.loading = false
-        console.log(err.response.data.message)
-      })
+        .catch(err => {
+          this.loading = false
+          console.log(err.response.data.message)
+        })
     },
     doEdit() {
-      edit(this.form).then(res => {
-        this.resetForm()
-        this.$notify({
-          title: '修改成功',
-          type: 'success',
-          duration: 2500
+      edit(this.form)
+        .then(res => {
+          this.resetForm()
+          this.$notify({
+            title: '修改成功',
+            type: 'success',
+            duration: 2500
+          })
+          this.loading = false
+          this.$parent.init()
         })
-        this.loading = false
-        this.$parent.init()
-      }).catch(err => {
-        this.loading = false
-        console.log(err.response.data.message)
-      })
+        .catch(err => {
+          this.loading = false
+          console.log(err.response.data.message)
+        })
     },
     resetForm() {
       this.dialog = false
       this.$refs['form'].resetFields()
+      this.copyPersonId = null
       this.form = {
-        id: '',
         todoAbstract: '',
         expectedCompletionTime: '',
-        copyPersonName: '',
-        assistantName: '',
-        content: '',
-        createTime: '',
-        updateTime: '',
-        deleted: ''
+        copyPerson: {
+          id: ''
+        },
+        content: ''
       }
+    },
+    getUsers() {
+      getUsers({
+        enabled: true
+      })
+        .then(res => {
+          this.copyPerson = res.content
+          this.assistantPerson = res.content
+        })
+        .catch(err => {
+          console.log(err.response.data.message)
+        })
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/views/documents/todoList/index.vue
+++ src/views/documents/todoList/index.vue
@@ -3,30 +3,24 @@
     <!--工具栏-->
     <div class="head-container">
       <!-- 搜索 -->
-      <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery"/>
+      <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="toQuery" />
       <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
-        <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key"/>
+        <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
       </el-select>
       <el-button class="filter-item" size="mini" type="success" icon="el-icon-search" @click="toQuery">搜索</el-button>
       <!-- 新增 -->
       <div style="display: inline-block;margin: 0px 2px;">
-        <el-button
-          v-permission="['ADMIN','TODOLIST_ALL','TODOLIST_CREATE']"
-          class="filter-item"
-          size="mini"
-          type="primary"
-          icon="el-icon-plus"
-          @click="add">新增</el-button>
+        <el-button v-permission="['ADMIN', 'TODOLIST_ALL', 'TODOLIST_CREATE']" class="filter-item" size="mini" type="primary" icon="el-icon-plus" @click="add">新增</el-button>
       </div>
     </div>
     <!--表单组件-->
-    <eForm ref="form" :is-add="isAdd"/>
+    <eForm ref="form" :is-add="isAdd" />
     <!--表格渲染-->
     <el-table v-loading="loading" :data="data" size="small" style="width: 100%;">
-      <el-table-column prop="todoAbstract" label="摘要"/>
-      <el-table-column prop="copyPerson.username" label="抄送人"/>
-      <el-table-column prop="assistantPerson.username" label="协助人员"/>
-      <el-table-column prop="content" label="待办内容"/>
+      <el-table-column prop="todoAbstract" label="摘要" />
+      <el-table-column prop="copyPerson.username" label="抄送人" />
+      <el-table-column prop="assistantPerson.username" label="协助人员" />
+      <el-table-column prop="content" label="待办内容" />
       <el-table-column prop="createTime" label="创建时间" width="135px">
         <template slot-scope="scope">
           <span>{{ parseTime(scope.row.createTime) }}</span>
@@ -37,20 +31,16 @@
           <span>{{ parseTime(scope.row.expectedCompletionTime) }}</span>
         </template>
       </el-table-column>
-      <el-table-column v-if="checkPermission(['ADMIN','TODOLIST_ALL','TODOLIST_EDIT','TODOLIST_DELETE'])" label="操作" width="150px" align="center">
+      <el-table-column v-if="checkPermission(['ADMIN', 'TODOLIST_ALL', 'TODOLIST_EDIT', 'TODOLIST_DELETE'])" label="操作" width="150px" align="center">
         <template slot-scope="scope">
-          <el-button v-permission="['ADMIN','TODOLIST_ALL','TODOLIST_EDIT']" size="mini" type="primary" icon="el-icon-edit" @click="edit(scope.row)"/>
-          <el-popover
-            v-permission="['ADMIN','TODOLIST_ALL','TODOLIST_DELETE']"
-            :ref="scope.row.id"
-            placement="top"
-            width="180">
+          <el-button v-permission="['ADMIN', 'TODOLIST_ALL', 'TODOLIST_EDIT']" size="mini" type="primary" icon="el-icon-edit" @click="edit(scope.row)" />
+          <el-popover v-permission="['ADMIN', 'TODOLIST_ALL', 'TODOLIST_DELETE']" :ref="scope.row.id" placement="top" width="180">
             <p>确定删除本条数据吗？</p>
             <div style="text-align: right; margin: 0">
               <el-button size="mini" type="text" @click="$refs[scope.row.id].doClose()">取消</el-button>
               <el-button :loading="delLoading" type="primary" size="mini" @click="subDelete(scope.row.id)">确定</el-button>
             </div>
-            <el-button slot="reference" type="danger" icon="el-icon-delete" size="mini"/>
+            <el-button slot="reference" type="danger" icon="el-icon-delete" size="mini" />
           </el-popover>
         </template>
       </el-table-column>
@@ -62,7 +52,8 @@
       style="margin-top: 8px;"
       layout="total, prev, pager, next, sizes"
       @size-change="sizeChange"
-      @current-change="pageChange"/>
+      @current-change="pageChange"
+    />
   </div>
 </template>
 
@@ -104,51 +95,57 @@ export default {
       const query = this.query
       const type = query.type
       const value = query.value
-      if (type && value) { this.params[type] = value }
+      if (type && value) {
+        this.params[type] = value
+      }
       return true
     },
     subDelete(id) {
       this.delLoading = true
-      del(id).then(res => {
-        this.delLoading = false
-        this.$refs[id].doClose()
-        this.dleChangePage()
-        this.init()
-        this.$notify({
-          title: '删除成功',
-          type: 'success',
-          duration: 2500
+      del(id)
+        .then(res => {
+          this.delLoading = false
+          this.$refs[id].doClose()
+          this.dleChangePage()
+          this.init()
+          this.$notify({
+            title: '删除成功',
+            type: 'success',
+            duration: 2500
+          })
         })
-      }).catch(err => {
-        this.delLoading = false
-        this.$refs[id].doClose()
-        console.log(err.response.data.message)
-      })
+        .catch(err => {
+          this.delLoading = false
+          this.$refs[id].doClose()
+          console.log(err.response.data.message)
+        })
     },
     add() {
       this.isAdd = true
       this.$refs.form.dialog = true
     },
     edit(data) {
+      debugger
       this.isAdd = false
       const _this = this.$refs.form
       _this.form = {
         id: data.id,
         todoAbstract: data.todoAbstract,
         expectedCompletionTime: data.expectedCompletionTime,
-        copyPersonName: data.copyPerson.username,
-        assistantPersonName: data.assistantPerson.username,
+        copyPerson: { id: data.copyPerson.id },
+        assistantPerson: { id: data.assistantPerson === null ? '' : data.assistantPerson.id },
         content: data.content,
         createTime: data.createTime,
         updateTime: data.updateTime,
         deleted: data.deleted
       }
+      _this.copyPersonId = data.copyPerson.id
+      _this.assistantPersonId = data.assistantPerson === null ? '' : data.assistantPerson.id
+      _this.getUsers()
       _this.dialog = true
     }
   }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
